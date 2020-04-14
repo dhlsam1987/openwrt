9c419b36ec34c7ee660bb65b72c939968d471e23
dhlsam1987 <63229268+dhlsam1987@users.noreply.github.com>
2020-04-15 01:25:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,7 @@
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #添加lienol包
 git clone https://github.com/Lienol/openwrt-package package/lienol/
+git clone https://github.com/pymumu/smartdns package/smartdns
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
