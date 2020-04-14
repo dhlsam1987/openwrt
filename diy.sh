36895d10b98648724ff7a9fec141e26a5e3aa4de
dhlsam1987 <63229268+dhlsam1987@users.noreply.github.com>
2020-04-14 19:10:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,11 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardhome
+git https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall package/passwall
+./scripts/feeds update -a
+./scripts/feeds install -a
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.11/g' package/base-files/files/bin/config_generate
