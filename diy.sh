43e52f06017b262e105c6345baac9af162f8cfe3
dhlsam1987 <63229268+dhlsam1987@users.noreply.github.com>
2020-04-15 09:40:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,15 +5,31 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.11/g' package/base-files/files/bin/config_generate
 #添加adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #添加lienol包
 git clone https://github.com/Lienol/openwrt-package package/lienol/
-git clone https://github.com/pymumu/smartdns package/smartdns
+
+WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+mkdir $WORKINGDIR -p
+rm $WORKINGDIR/* -fr
+wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
+unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
+rmdir $WORKINGDIR/openwrt-smartdns-master
+rm $WORKINGDIR/master.zip
+
+LUCIBRANCH="master" #更换此变量
+WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
+mkdir $WORKINGDIR -p
+rm $WORKINGDIR/* -fr
+wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
+unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
+mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
+rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
+rm $WORKINGDIR/${LUCIBRANCH}.zip
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
-# Modify default IP
-sed -i 's/192.168.1.1/10.10.10.11/g' package/base-files/files/bin/config_generate
